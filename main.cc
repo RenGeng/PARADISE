ebd4d4496d65b8262f59b9f545e7b48571586ddd
lance <lancesatge@hotmail.fr>
2017-12-05 12:39:32 +0100

    V2

--- main.cc
+++ main.cc
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <unistd.h>
 
+using namespace std;
 
 int main()
 {
@@ -11,10 +13,27 @@ int main()
     //Modifier position de la fenêtre
     window.setPosition(sf::Vector2i(200,200)); //A modifier selon les écrans
     window.setFramerateLimit(60);
-    sf::CircleShape cercle;
-    cercle.setFillColor(sf::Color(100,250,50));
-    cercle.setRadius(50);
-    cercle.setPosition(100,100);
+    //Background
+    sf::Texture perso;
+	sf::Sprite cercle;
+	if(!perso.loadFromFile("personnage.jpg",sf::IntRect(30,30,350,430)))
+	{
+		// Erreur
+		cout << "Erreur durant le chargement de l'image de background." << endl;
+	}
+	else{
+		perso.setSmooth(true);
+		cercle.setTexture(perso);
+	}
+	cercle.setScale(0.25,0.25);
+	window.draw(cercle);
+
+    // sf::CircleShape cercle;
+    // cercle.setFillColor(sf::Color(100,250,50));
+    // cercle.setRadius(50);
+    // cercle.setPosition(100,100);
+    window.clear();
+    window.draw(cercle);
 
     while(window.isOpen()){
     	sf::Event event;
@@ -22,6 +41,19 @@ int main()
     		//Si on clique sur fermer
     		if(event.type == sf::Event::Closed) window.close();
     	}
+
+    	//Gestion des évènements
+    	int vitesse=10;
+    	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) cercle.move(0,-vitesse);
+    	else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) cercle.move(0,vitesse);
+    	else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) cercle.move(vitesse,0);
+    	else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) cercle.move(-vitesse,0);
+    	else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) window.clear();
+    	else if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+    		window.clear();
+    		window.draw(cercle);
+    		cercle.setPosition(sf::Mouse::getPosition(window).x,sf::Mouse::getPosition(window).y);
+    	} 
     	window.draw(cercle);
     	window.display();
     	window.clear();
